bca63654c8f1bede16c4802b480419b3a5a5c0e6
fuyunjinglong <806435328@qq.com>
2025-04-05 19:57:53 +0800

    vant

---
+++ typings/vue-shim.d.ts
@@ -0,0 +1,7 @@
+// 用于标记识别后缀vue文件
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
